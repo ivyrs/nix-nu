0e818bfca9254e8b0202fdd88065e7f14a6714d0
ivy forever <ivy@ivy.rs>
2026-08-20 23:55:17 +0100

    home: add ghostty

---
+++ home/ghostty.nix
@@ -0,0 +1,38 @@
+# home/ghostty.nix
+{ lib, pkgs, ... }:
+
+{
+  programs.ghostty = {
+    enable = true;
+
+    package =
+      if pkgs.stdenv.hostPlatform.isDarwin
+      then pkgs.ghostty-bin
+      else pkgs.ghostty;
+
+    systemd.enable = false;
+
+    settings = lib.mkMerge [
+      {
+        # Noctalia can override this later.
+        theme = lib.mkDefault "Catppuccin Mocha";
+
+        font-family = "Aporetic Sans Mono";
+        font-size = 14;
+        background-opacity = 0.90;
+        cursor-style = "block";
+        window-padding-x = 10;
+        window-padding-y = 10;
+        confirm-close-surface = false;
+
+        keybind = [
+          "super+v=paste_from_clipboard"
+        ];
+      }
+
+      (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
+        macos-titlebar-style = "tabs";
+      })
+    ];
+  };
+}
