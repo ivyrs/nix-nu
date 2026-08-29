85de398c0659dd12d54d22503fe7c0ffd6340a97
Xela <git@xela.zone>
2026-08-28 16:01:13 -0800

    feat(system/nix): add comma via nix-index-database

--- flake.nix
+++ flake.nix
@@ -50,6 +50,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
