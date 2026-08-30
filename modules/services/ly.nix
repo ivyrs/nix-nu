d2732e6c5cd45e0b8d2f084d6c54669edadbf31b
ivy forever <ivy@ivy.rs>
2026-08-30 08:18:36 +0100

    services: ly gets a facelift

--- modules/services/ly.nix
+++ modules/services/ly.nix
@@ -1,6 +1,20 @@
 {
   services.displayManager.ly = {
     enable = true;
-    settings.session_log = null;
+    settings = {
+      session_log = null;
+      auth_fails = 5;
+
+      animation = "matrix";
+      blank_box = true;
+      box_title = null;
+      clock = "%A, %B %d  —  %H:%M:%S";
+      hide_key_hints = true;
+
+      fg = "0x00FFFFFF";
+      border_fg = "0x00FFFFFF";
+      cmatrix_fg = "0x00ADD8E6";
+      cmatrix_head_col = "0x01FFFFFF";
+    };
   };
 }
